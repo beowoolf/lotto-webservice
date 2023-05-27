ed49453c38dedb320b7730f0b8726b38249e6eac
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2023-05-27 14:53:16 +0200

    Removed unnecessary @Repository annotations

--- src/main/java/pl/gry/lotto/domain/numbergenerator/WinningNumbersRepository.java
+++ src/main/java/pl/gry/lotto/domain/numbergenerator/WinningNumbersRepository.java
@@ -1,12 +1,10 @@
 package pl.gry.lotto.domain.numbergenerator;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-@Repository
 public interface WinningNumbersRepository extends MongoRepository<WinningNumbers, String> {
 
     Optional<WinningNumbers> findNumbersByDate(LocalDateTime date);
--- src/main/java/pl/gry/lotto/domain/resultchecker/PlayerRepository.java
+++ src/main/java/pl/gry/lotto/domain/resultchecker/PlayerRepository.java
@@ -1,11 +1,9 @@
 package pl.gry.lotto.domain.resultchecker;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface PlayerRepository extends MongoRepository<Player, String> {
 
     Optional<Player> findById(String hash);
